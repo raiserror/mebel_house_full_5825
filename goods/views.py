1e50652ce3bab013009f12c0f80dc56c24b1281d
ByTryHard <erroraccount@bk.ru>
2025-08-08 19:16:55 +0400

    Category new slug url, goods not found

--- goods/views.py
+++ goods/views.py
@@ -4,15 +4,30 @@ from .models import Categories
 from goods.models import Products
 
 # Create your views here.
-def catalog(request):
+def catalog(request, category_slug):
+    if category_slug == 'all':
+        goods = Products.objects.all()
+    else:
+        goods = Products.objects.filter(category__slug=category_slug)
+    
     categories = Categories.objects.all()  
-    goods = Products.objects.all()
+    
+    # Проверяем, пустой ли список товаров
+    is_empty = not goods.exists()
+    
     context = {
         "categories": categories,
         "goods": goods,
+        "is_empty": is_empty,  # Флаг для шаблона
     }
     
     return render(request, 'goods/catalog.html', context)
 
-def product(request):
-    return render(request, 'goods/product.html')
+def product(request, product_slug):
+    product = Products.objects.get(slug=product_slug)
+    
+    context = { 
+        'product': product,
+    }
+    
+    return render(request, 'goods/product.html', context=context)
