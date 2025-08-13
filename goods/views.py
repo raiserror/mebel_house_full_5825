22169dcafabace6f73372527539e019642fcd54b
raiserror <159466221+raiserror@users.noreply.github.com>
2025-08-13 22:18:10 +0400

    Сorrect urlencode + search

--- goods/views.py
+++ goods/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import get_list_or_404, render
 from django.template import context
+
+from goods.utils import q_search
 from .models import Categories
 from goods.models import Products
 from django.core.paginator import Paginator
@@ -8,11 +10,22 @@ from django.core.paginator import Paginator
 def catalog(request, category_slug=None):
     
     page = request.GET.get('page', 1)
+    on_sale = request.GET.get('on_sale', None)
+    order_by = request.GET.get('order_by', None)
+    query = request.GET.get('q', None)
     
     if category_slug == 'all':
         goods = Products.objects.all()
+    elif query:
+        goods = q_search(query)
     else:
         goods = Products.objects.filter(category__slug=category_slug)
+        
+    if on_sale:
+        goods = goods.filter(discount__gt=0)    
+    
+    if order_by and order_by != 'default':
+        goods = goods.order_by(order_by) 
         
     paginator = Paginator(goods, 6)    
     current_page = paginator.page(int(page))
